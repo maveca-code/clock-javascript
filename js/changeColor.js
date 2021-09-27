a08cc990d808a24d3e112f9dcfc536eb6eec29a3
Mario Velasco Cañadas <mariovelascocanadas@outlook.es>
2021-09-27 17:30:46 +0200

    Change background clock

---
+++ js/changeColor.js
@@ -0,0 +1,16 @@
+'use strict'
+
+window.addEventListener('load', () => {
+
+    /*Elements DOM */
+    const check = document.querySelector('.toggle');
+    const body = document.querySelector('.body');
+
+    check.addEventListener('click', function(){
+        if(this.checked){
+            body.classList.add('change-color');
+        }else{
+            body.classList.remove('change-color');
+        }
+    });
+});
